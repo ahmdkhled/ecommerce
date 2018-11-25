a858a16a26c6a7e2e7440247e44989b2af7b5afd
ibrahim karim othman <ibrahimothmanre@gmail.com>
2018-11-25 17:16:50 +0200

    Add retrofit dependency

---
+++ app/src/main/java/com/ahmdkhled/ecommerce/ui/RegistrationActivity.java
@@ -0,0 +1,51 @@
+package com.ahmdkhled.ecommerce.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.ahmdkhled.ecommerce.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.email_edittext_reg)
+    AppCompatEditText mEmailTxt;
+    @BindView(R.id.password_edittext_reg)
+    AppCompatEditText mPasswordTxt;
+    @BindView(R.id.btn_signup)
+    AppCompatButton mRegisterBtn;
+    @BindView(R.id.login_txt)
+    TextView mLoginTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        // bind views
+        ButterKnife.bind(this);
+
+        mRegisterBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_signup){
+            String userEmail = mEmailTxt.getText().toString();
+            String userPassword = mPasswordTxt.getText().toString();
+            if(!TextUtils.isEmpty(userEmail) && !TextUtils.isEmpty(userPassword)){
+
+            }else {
+                Toast.makeText(this, R.string.info_lack, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
