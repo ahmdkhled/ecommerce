19073c3c58d2165129db93a737492ef8ecadd3f9
ibrahim karim othman <ibrahimothmanre@gmail.com>
2018-11-27 13:37:12 +0200

    Add signup retrofit interface
    - Add signup function in ApiService
    - Create Constants class
    - Add signup url in Constant class

--- app/src/main/java/com/ahmdkhled/ecommerce/network/ApiService.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/network/ApiService.java
@@ -1,4 +1,20 @@
 package com.ahmdkhled.ecommerce.network;
 
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public class ApiService {
+
+    public interface ApiInterface{
+
+        @FormUrlEncoded
+        @POST(Constants.SIGNUP_URL)
+        public Call<String> signup(@FieldMap HashMap<String,String> map);
+    }
+
+
 }
--- app/src/main/java/com/ahmdkhled/ecommerce/network/RetrofetClient.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/network/RetrofetClient.java
@@ -2,16 +2,17 @@ package com.ahmdkhled.ecommerce.network;
 
 import retrofit2.Retrofit;
 
+
 public class RetrofetClient {
 
-    public static final String BaseUrl="https://ecommerceg.000webhostapp.com";
+
 
     private static Retrofit retrofit;
     private static ApiService ApiService;
 
     private static Retrofit getInstance(){
         if (retrofit==null){
-            retrofit=new Retrofit.Builder().baseUrl(BaseUrl).build();
+            retrofit=new Retrofit.Builder().baseUrl(Constants.BASE_URL).build();
         }
         return retrofit;
     }
