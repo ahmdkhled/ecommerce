d2f6fdb6adac8701e8187598eed510139b01e1be
Salwa AbdEl-hady <salwaabdelhady2013@gmail.com>
2018-11-27 14:43:40 +0200

    design cart activity

---
+++ app/src/main/java/com/ahmdkhled/ecommerce/CartActivity.java
@@ -0,0 +1,14 @@
+package com.ahmdkhled.ecommerce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CartActivity extends AppCompatActivity {
+     int quantity ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
