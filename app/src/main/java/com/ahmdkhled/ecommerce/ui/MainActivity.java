8dfa5cfa84aeea7597d5bb336930869a285f8ac3
ahmdkhled <ahmdkhled@gmail.com>
2018-11-29 15:37:57 +0200

    add navigation  drawer to main Activity

--- app/src/main/java/com/ahmdkhled/ecommerce/ui/MainActivity.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/ui/MainActivity.java
@@ -1,15 +1,61 @@
 package com.ahmdkhled.ecommerce.ui;
 
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.ahmdkhled.ecommerce.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar=findViewById(R.id.mainToolbar);
+        drawerLayout=findViewById(R.id.mainDrawerLayout);
+        NavigationView navigationView=findViewById(R.id.mainNavView);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,
+                toolbar,R.string.open_drawer, R.string.close_drawer);
+        toggle.syncState();
+        toggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(getApplicationContext()," "+item.getTitle(),Toast.LENGTH_SHORT).show();
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 }
