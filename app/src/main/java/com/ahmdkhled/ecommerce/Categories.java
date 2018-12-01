eea3ead65f8b3e607345e4e99732cd0a602aa77b
Amr <amribrahimgazala@gmail.com>
2018-12-01 13:45:54 +0200

    Add Category Activity

---
+++ app/src/main/java/com/ahmdkhled/ecommerce/Categories.java
@@ -0,0 +1,18 @@
+package com.ahmdkhled.ecommerce;
+
+public class Categories  {
+    String image;
+
+    public Categories(String image) {
+
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/com/ahmdkhled/ecommerce/MainActivity.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/MainActivity.java
@@ -1,13 +1,29 @@
 package com.ahmdkhled.ecommerce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.image);
+
+        Picasso.get().load("https://ecommerceg.000webhostapp.com/media/p.jpg").into(imageView);
+
+
+        //Intent intent = new Intent(this,CategoriesActivity.class);
+        //startActivity(intent);
+
     }
 }
