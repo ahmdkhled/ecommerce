95130bc5fd888d4fe5c6a4190967fd398e36f387
amirahesham <amirahesham2018.com>
2018-12-01 16:25:29 +0200

    make design of product detail activity

--- app/src/main/java/com/ahmdkhled/ecommerce/MainActivity.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/MainActivity.java
@@ -1,13 +1,22 @@
 package com.ahmdkhled.ecommerce;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+            }
+
 }
