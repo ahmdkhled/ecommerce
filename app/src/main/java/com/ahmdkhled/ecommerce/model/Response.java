bc80ba7a141ed39780ba9091f291cfd4b5e8d3be
ahmdkhled <ahmdkhled@gmail.com>
2019-01-24 21:16:40 +0200

    save user session after successful login

--- app/src/main/java/com/ahmdkhled/ecommerce/model/Response.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/model/Response.java
@@ -3,11 +3,16 @@ package com.ahmdkhled.ecommerce.model;
 public class Response {
     private boolean error;
     private String message;
+    private long id;
+    private String name;
+    private String email;
 
-    public Response(boolean error, String message) {
-
+    public Response(boolean error, String message, long id, String name, String email) {
         this.error = error;
         this.message = message;
+        this.id = id;
+        this.name = name;
+        this.email = email;
     }
 
     public boolean isError() {
@@ -26,5 +31,27 @@ public class Response {
         this.message = message;
     }
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
