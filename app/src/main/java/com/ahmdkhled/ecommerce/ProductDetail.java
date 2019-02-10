ecc969a2c1860e3d9c1a13ab575028a36b60ac79
amira hesham <amiraelhoseny2018@gmail.com>
2019-02-10 15:22:08 +0200

    add toolbar in product detail activity

--- app/src/main/java/com/ahmdkhled/ecommerce/ProductDetail.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/ProductDetail.java
@@ -1,10 +1,9 @@
 package com.ahmdkhled.ecommerce;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -12,21 +11,26 @@ import android.widget.TextView;
 public class ProductDetail extends AppCompatActivity {
     TextView textView;
     Button addToCart;
-    SharedPreferences sharedPreferences;
+    Toolbar toolbar;
+    ViewPager viewPager;
+    ProductsImagesPagerAdapter productsImagesPagerAdapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_detail);
-        textView = (TextView) findViewById(R.id.product_name);
-        addToCart =(Button) findViewById(R.id.addToCart);
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        viewPager = findViewById(R.id.productImagesPager);
+        productsImagesPagerAdapter = new ProductsImagesPagerAdapter(this);
+        viewPager.setAdapter(productsImagesPagerAdapter);
+        textView = findViewById(R.id.product_name);
+        addToCart = findViewById(R.id.addToCart);
         addToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sharedPreferences = getSharedPreferences("sendData", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.apply();
+
             }
             });
     }
