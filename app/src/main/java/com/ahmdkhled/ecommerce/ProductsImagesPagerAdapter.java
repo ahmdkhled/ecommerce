21ae8071c51f33ca22db8179f38aee8f80a006b6
amira hesham <amiraelhoseny2018@gmail.com>
2019-02-10 15:13:45 +0200

    implememt productsImagePagerAdapter im products detail activity

---
+++ app/src/main/java/com/ahmdkhled/ecommerce/ProductsImagesPagerAdapter.java
@@ -0,0 +1,51 @@
+package com.ahmdkhled.ecommerce;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class ProductsImagesPagerAdapter extends PagerAdapter {
+    private Context context;
+    LayoutInflater inflater;
+    public int[] images = {
+            R.drawable.picture,
+//            R.drawable.picture2,
+    };
+    public ProductsImagesPagerAdapter(Context context){
+        this.context = context;
+
+    }
+    @Override
+    public int getCount(){
+        return images.length;
+
+    }
+    @Override
+    public boolean isViewFromObject(View view, Object object){
+        return (view==(LinearLayout)object);
+    }
+
+
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position){
+        inflater =inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.product_images_layout,container,false);
+        ImageView image =  view.findViewById(R.id.productSliderImg);
+        image.setImageResource(images[position]);
+        container.addView(view);
+        return view;
+    }
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+
+    }
+
+}
