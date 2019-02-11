39e33d7ab140e01cbae9e8cbf401af982eceeea0
ahmdkhled <ahmdkhled@gmail.com>
2019-02-11 15:35:17 +0200

    handle add to cart button in product detail activity

--- app/src/main/java/com/ahmdkhled/ecommerce/ui/ProductDetail.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/ui/ProductDetail.java
@@ -8,9 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ahmdkhled.ecommerce.ProductsImagesPagerAdapter;
 import com.ahmdkhled.ecommerce.R;
+import com.ahmdkhled.ecommerce.model.Product;
+import com.ahmdkhled.ecommerce.utils.CartItemsManger;
 
 public class ProductDetail extends AppCompatActivity {
     TextView textView;
@@ -18,9 +21,8 @@ public class ProductDetail extends AppCompatActivity {
     Toolbar toolbar;
     ViewPager viewPager;
     ProductsImagesPagerAdapter productsImagesPagerAdapter;
-    SharedPreferences sharedPreferences;
     public static final String PRODUCT_KEY="product_key";
-
+    Product product;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +35,23 @@ public class ProductDetail extends AppCompatActivity {
         viewPager.setAdapter(productsImagesPagerAdapter);
         textView = findViewById(R.id.product_name);
         addToCart = findViewById(R.id.addToCart);
+
+        if (getIntent()!=null &getIntent().hasExtra(PRODUCT_KEY)){
+            product=getIntent().getParcelableExtra(PRODUCT_KEY);
+        }
         addToCart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if (product!=null){
+                    Toast.makeText(getApplicationContext(),"added to cart ",Toast.LENGTH_SHORT).show();
+                    CartItemsManger cartItemsManger=new CartItemsManger(getApplicationContext());
+                    cartItemsManger.saveCartItem(product.getId(),1);
+                }else{
+                    Toast.makeText(getApplicationContext(),"error ",Toast.LENGTH_SHORT).show();
+                }
             }
             });
     }
+
+
 }
