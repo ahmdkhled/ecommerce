da8bf1c68f50328bad39b050b9ac4c91025ccc2f
Amr <amribrahimgazala@gmail.com>
2019-03-09 13:22:35 +0200

    add misiing import of constraint layout

--- app/src/main/java/com/ahmdkhled/ecommerce/ui/AccountActivity.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/ui/AccountActivity.java
@@ -1,6 +1,7 @@
 package com.ahmdkhled.ecommerce.ui;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
