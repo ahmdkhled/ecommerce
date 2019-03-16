03afc59eae88aba39e8e2e1531053806597e46d4
Amr <amribrahimgazala@gmail.com>
2019-03-16 15:41:30 +0200

    Remove some conflicts

--- app/src/main/java/com/ahmdkhled/ecommerce/ui/AddressActivity.java
+++ app/src/main/java/com/ahmdkhled/ecommerce/ui/AddressActivity.java
@@ -182,7 +182,7 @@ public class AddressActivity extends AppCompatActivity {
             }
         });
     }
-
+fv
     public void showProgressBar(){
         mProgressBar.setVisibility(View.VISIBLE);
     }
